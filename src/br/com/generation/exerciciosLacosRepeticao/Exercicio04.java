fdcca3362f4c97893c7f1b40b6aa1c86ecbac0ac
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-09-04 21:50:19 -0300

    Concluindo Exercício 04 de Laços de Repetição

--- src/br/com/generation/exerciciosLacosRepeticao/Exercicio04.java
+++ src/br/com/generation/exerciciosLacosRepeticao/Exercicio04.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 
 /* Programa: Exercicio 04
  * Autor: Patrícia
- * Data: 02/09/2021
+ * Data: 04/09/2021
  */
 
-
-//4-	Uma empresa desenvolveu uma pesquisa para saber as características psicológicas dos indivíduos de uma região. Para tanto,
-//a cada uma das pessoas era perguntado: idade, sexo (1-feminino / 2-masculino / 3-Outros), e as opções
-//(1, se a pessoa era calma; 2, se a pessoa era nervosa e 3, se a pessoa era agressiva).
-//Pede-se para elaborar um sistema que permita ler os dados de 150 pessoas, calcule e mostre: 
 public class Exercicio04 {
 
 	public static void main(String[] args) {
@@ -20,9 +15,15 @@ public class Exercicio04 {
 	
 	int idade, sexo, caracteristicas;
 	int contador = 0;
-	int contadorFeminino = 0;
+	int contFemininoNervosa = 0;
+	int contMasculinoAgressivo = 0;
+	int contPessoaCalma=0;
+	int contOutrosCalmos=0;
+	int contPessoasNervosasMais40=0;
+	int contPessoasCalmaMenos18=0;
 	
-	while(contador <= 3) {
+	
+	while(contador <= 7) {
 	
 	System.out.println("Digite sua idade: ");	
 	idade = leia.nextInt();
@@ -30,17 +31,77 @@ public class Exercicio04 {
 	System.out.println("Digite seu sexo (1-Feminino / 2-Masculino / 3-Outros): ");
 	sexo = leia.nextInt();
 	
+	System.out.println("Digite o numero correspondente a sua caracteristica (1-Calma, 2 -Nervosa, 3-Agressiva: ");
+	caracteristicas = leia.nextInt();
+	
 	switch (sexo) {
 	case 1:
-		contadorFeminino++;
+		if(caracteristicas==1) {
+			contPessoaCalma++;
+		}
+		
+		if(caracteristicas==2) {
+			contFemininoNervosa++;
+		}
+		
+		if(idade > 40 && caracteristicas==2) {
+			contPessoasNervosasMais40++;
+		}
+		
+		if(idade < 18 && caracteristicas==1) {
+			contPessoasCalmaMenos18++;
+		}
+		
 		break;
-
+	case 2:
+		if(caracteristicas==1) {
+			contPessoaCalma++;
+		}
+		
+		if(caracteristicas==3) {
+			contMasculinoAgressivo++;
+		}
+		
+		if(idade > 40 && caracteristicas==2) {
+			contPessoasNervosasMais40++;
+		}
+		
+		if(idade < 18 && caracteristicas==1) {
+			contPessoasCalmaMenos18++;
+		}
+		
+		break;
+	case 3:
+		if(caracteristicas==1) {
+			contPessoaCalma++;
+			contOutrosCalmos++;
+		}
+		
+		if(idade > 40 && caracteristicas==2) {
+			contPessoasNervosasMais40++;
+		}
+		
+		if(idade < 18 && caracteristicas==1) {
+			contPessoasCalmaMenos18++;
+		}
+		break;
 	default:
+		System.out.println("Valor informado para sexo inválido!!");
 		break;
 	}
+	
+	contador++;
 		
 	}
+	
+	System.out.println("O número de pessoas calmas é: " + contPessoaCalma); 
+	System.out.println("O número de mulheres nervosas é: " + contFemininoNervosa); 
+	System.out.println("O número de homens agressivos é: " + contMasculinoAgressivo);
+	System.out.println("O número de outros calmos é: " + contOutrosCalmos);
+	System.out.println("O número de pessoas nervosas com mais de 40 anos é: " + contPessoasNervosasMais40);
+	System.out.println("O número de pessoas calmas com menos de 18 anos é: " + contPessoasCalmaMenos18);
 
+	leia.close();
 	}
 
 }
